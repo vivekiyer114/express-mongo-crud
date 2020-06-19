eb63cd0d400e9b8bc17fe1b0f2b2ee12bf8abe82
Vivek <vivek.iyer@rebelfoods.co>
2020-06-19 13:14:44 +0530

    model routes

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,17 +1,11 @@
 const rateLimit = require("express-rate-limit");
 
 
-const imageLimiter = rateLimit({
+const rateLimiter = rateLimit({
     windowMs:60000, // 1 hour
     max:3,
     message:'Too many API requests created from this IP. Please try again later!'
 });
 
-const artistLimiter = rateLimit({
-    windowMs:60000, // 1 hour
-    max:5,
-    message:'Too many registrations from this IP. Please try again later!'
-})
 
-
-module.exports = {imageLimiter,artistLimiter};
+module.exports.rateLimiter = rateLimiter;
---
+++ routes/model.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const crud = require('../middlewares/crud');
+const acl = require('../middlewares/acl');
+const models = require('../models');
+const modelNames = fs.readdirSync('./models');
+
+module.exports = app => {
+    modelNames.forEach(name => {
+        let formattedName = name.replace('.js','').toLowerCase();
+        app.use(`/${formattedName}`,acl,crud(models[name]));
+    })
+}
